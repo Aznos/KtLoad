742ae8d52e4eebab09fbab3bd1cdbce43a66e79a
aznos <aznosreal@gmail.com>
2025-11-23 14:52:18 -0500

    Clikt

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("com.github.ajalt.clikt:clikt:5.0.1")
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.1")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 package com.maddoxh
 
-fun main() {
-    println("Hello")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
+
+class KtLoad : CliktCommand() {
+    val count: Int by option().int().default(100).help("Number of requests")
+
+    override fun run() {
+        repeat(count) {
+            println("Sending request to server")
+        }
+    }
 }
+
+fun main(args: Array<String>) = KtLoad().main(args)
